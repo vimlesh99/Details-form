65c103534fe176f073f9c2b1f65484a9ad24f1db
Vimlesh <vimileshkumar99gkp@gmail.com>
2022-08-21 20:38:14 +0530

    address component complete

---
+++ src/component/Address.js
@@ -0,0 +1,86 @@
+/** @format */
+
+import { Country, State, City } from "country-state-city/lib/cjs";
+import { useState } from "react";
+
+const Address = () => {
+
+  console.log(City.getCitiesOfState("IN", "DL"));
+
+  const [data, setData] = useState("IN");
+  console.log(data);
+
+  const getCountryCode = (event) => {
+    console.log(event.target.value);
+    let value = event.target.value;
+    let myData = Country.getAllCountries();
+    console.log(myData);
+    let countryCode = myData.filter((elm) => {
+      if (elm.name === value) {
+        return elm.isoCode;
+      }
+    });
+    let code = countryCode[0].isoCode;
+    setData(code);
+  };
+
+  return (
+    <div>
+      <h6>Address</h6>
+      <div>
+        <label htmlFor="address">Address</label>
+        <input
+          type="number"
+          id="address"
+          nammxxxyy="address"
+          autoComplete="false"
+          value=""
+        />
+
+        <label htmlFor="state">State</label>
+        <select name="state" id="state" class="form-control">
+          {State.getStatesOfCountry(data).map((elm) => (
+            <option value="Andaman and Nicobar Islands">{elm.name}</option>
+          ))}
+        </select>
+
+        <label htmlFor="city">City</label>
+        <select name="city" id="city">
+          {City.getCitiesOfState("IN", "DL").map((elm) => {
+           return <option key={elm.isoCode} value={elm.name} id={elm.isoCode}>
+              {elm.name}
+            </option>;
+          })}
+        </select>
+
+        <label htmlFor="country">Country</label>
+        <select name="country" id="country" onChange={getCountryCode}>
+          <option value="india" selected>
+            India
+          </option>
+          {Country.getAllCountries().map((elm) => {
+            return (
+              <>
+                <option key={elm.isoCode} value={elm.name} id={elm.isoCode}>
+                  {elm.name}
+                </option>
+              </>
+            );
+          })}
+        </select>
+
+        <label htmlFor="pincode">Pincode</label>
+        <input
+          type="number"
+          id="pincode"
+          name="pincode"
+          placeholder="enter the pincode"
+          autoComplete="false"
+          value=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Address;
--- src/component/Registration.js
+++ src/component/Registration.js
@@ -1,12 +1,104 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import Address from "./Address";
 
 const Registration = () => {
+ 
   return (
     <div>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="name"
+              id="name"
+              nammxxxyy="name"
+              autoComplete="false"
+              value=""
+            />
+          
+            <label htmlFor="date">Date of Birth or age</label>
+            <input
+              type="date"
+              id="date"
+              nammxxxyy="dob"
+              autoComplete="false"
+              value=""
+            />
+        <label htmlFor="gender">sex</label>
+            <select id="gender">
+              <option selected> Select Gender</option>
+              <option value="Other">Other</option>
+              <option value="male">male</option>
+              <option value="female">female</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="mobile">Mobile</label>
+            <input
+              type="number"
+              id="mobile"
+              nammxxxyy="mobile"
+              autoComplete="false"
+              value=""
+            />
+         
+            <label id="govt-id">Govt issue Id</label>
+            <select>
+              <option selected> Select Id</option>
+              <option value="Other">Other</option>
+              <option value="Adhar">Adhar</option>
+              <option value="Driving Licence">Driving Licence</option>
+              <option value="pan">PAN</option>
+            </select>
+            <input
+              type="text"
+              nammxxxyy="id-value"
+              autoComplete="false"
+              value=""
+            />
+       </div>
+
+          <h6>Contact details</h6>
+          <div>
+            <label htmlFor="guardian">Guardian Detail</label>
+            <select>
+              <option selected> Relation of Guardian</option>
+              <option value="mother">Mother</option>
+              <option value="Father">Father</option>
+              <option value="brother">Brother</option>
+            </select>
+            <input name="guardian-name" type="text" value="" />
       
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              nammxxxyy="email"
+              autoComplete="false"
+              value=""
+            />
+        
+            <label htmlFor="emargency">Emargency Contact Number</label>
+            <input
+              type="number"
+              id="emargency"
+              nammxxxyy="emargency_contact_number"
+              autoComplete="false"
+              value=""
+            />
+            <div>
+            <Address/>
+            </div>
+           
+            <h6>Other details</h6>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Registration
-
+export default Registration;
